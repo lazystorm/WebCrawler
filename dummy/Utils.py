fa942b37fbb14d96b799db6f88e3304b00cb6242
Storm <842222375@qq.com>
2016-12-06 17:00:25 +0800

    add wrapper for auto data save and load

---
+++ dummy/Utils.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+
+
+def auto_save_and_load(force_update=False):
+    def decorator(updater):
+        def wrapper(*args, **kw):
+            filename = kw['filename']
+            if not force_update and os.path.exists(filename):
+                with open(filename, 'rb') as fd:
+                    fd.readline()
+                    obj = json.load(fd, encoding='utf-8')
+            else:
+                obj = updater(*args, **kw)
+                with open(filename, 'wb') as fd:
+                    fd.write('# -*- coding: utf-8 -*-\n')
+                    json.dump(obj, fd, ensure_ascii=False)
+            return obj
+
+        return wrapper
+
+    return decorator
--- dummy/WebSpider.py
+++ dummy/WebSpider.py
@@ -2,6 +2,10 @@
 import requests
 import re
 import gevent
+import json
+import os
+import Utils
+
 
 class Spider3dm:
     def __init__(self, url):
@@ -14,56 +18,100 @@ class Spider3dm:
         if self.main_page is None:
             self.main_page = requests.get(self.main_url)
         end = pattern.findall(self.main_page.content)
-        return end[0][end[0].find('_')+1:end[0].find('.html')]
+        return end[0][end[0].find('_') + 1:end[0].find('.html')]
 
     def image_urls(self, page):
         """<p><a target="_blank" href="http://admin.3dmgame.com/uploads/allimg/161205/153_161205155056_2.jpg
         " style="text-indent: 24px;"><img src="http://admin.3dmgame.com/uploads/allimg/161205/153_161205155056_2_lit.jpg
         " border="0" alt="" /></a></p>
         <p>咖啡师初学者</p>"""
-        pattern = re.compile(r'<p><a target="_blank" href="http://admin.3dmgame.com/uploads/allimg/[0-9]+/.*?" style="text-indent: 24px;"><img src="http://admin.3dmgame.com/uploads/allimg/[0-9]+/.*?" border="0" alt="" /></a></p>.*?<p>.*?</p>', flags=re.DOTALL)
+        pattern = re.compile(
+            r'<p><a target="_blank" href="http://admin.3dmgame.com/uploads/allimg/[0-9]+/.*?" style="text-indent: 24px;"><img src="http://admin.3dmgame.com/uploads/allimg/[0-9]+/.*?" border="0" alt="" /></a></p>.*?<p>.*?</p>',
+            flags=re.DOTALL)
         urls = pattern.findall(page)
         imgs = []
         strs = []
         for url in urls:
-            imgs.append(str(url[url.find('href="http:') + len('href="'):url.find('" style="text-indent: 24px;"><img src=')]))
+            imgs.append(
+                str(url[url.find('href="http:') + len('href="'):url.find('" style="text-indent: 24px;"><img src=')]))
             strs.append(str(url[url.rfind('<p>') + len('<p>'):-4]))
         return imgs, strs
 
-    def get_all_url_str(self):
+    @Utils.auto_save_and_load()
+    def get_all_url_str(self, filename):
         if self.main_page is None:
             self.main_page = requests.get(self.main_url)
-        self.imgs, self.strs = self.image_urls(self.main_page.content)
-        for i in range(2, int(self.page_end())+1):
+        imgs, strs = self.image_urls(self.main_page.content)
+        for i in range(2, int(self.page_end()) + 1):
             url = self.main_url[:-5] + '_' + str(i) + self.main_url[-5:]
             page = requests.get(url)
             im, st = self.image_urls(page.content)
-            self.strs += st
-            self.imgs += im
-        return self.strs, self.imgs
+            strs += st
+            imgs += im
+        data = {
+            'strs': strs,
+            'imgs': imgs,
+        }
+        return data
 
     def download(self):
-        self.get_all_url_str()
+        murl = self.main_url
+        self.dirname = murl[murl.rfind('/') + 1:-5]
+        print self.dirname
+        if not os.path.exists(self.dirname):
+            os.makedirs(self.dirname)
+        os.chdir(self.dirname)
+        filename = self.dirname + '.py'
+        data = self.get_all_url_str(filename=filename)
+        self.strs = data['strs']
+        self.imgs = data['imgs']
         threads = []
         for url in self.imgs:
             threads.append(gevent.spawn(Spider3dm.download_image(url)))
         gevent.joinall(threads)
+        os.chdir('..')
 
     @staticmethod
     def download_image(url):
         filename = url[url.rfind(r'/') + 1:]
+        if os.path.exists(filename):
+            return
         r = requests.get(url)
-        Spider3dm.write_file(filename, r)
+        Spider3dm.write_file(filename, r.content)
 
     @staticmethod
     def write_file(filename, content):
         with open(filename, 'wb') as fd:
-            for chunk in content.iter_content(chunk_size=4096*1024):
-                fd.write(chunk)
+            fd.write(content)
+
+
+@Utils.auto_save_and_load()
+def get_join_pic_urls(main_url, filename):
+    totalpage = 164
+    params = {'tid': '151', 'totalpage': str(totalpage), 'page': '1'}
+    join_pic_urls = []
+    for i in range(totalpage):
+        params['page'] = str(i + 1)
+        r = requests.get(main_url, params=params)
+        pattern = re.compile(r'<a href="http://www.3dmgame.com/zt/[0-9]+/[0-9]+.html" target="_blank">.*?</a>',
+                             flags=re.DOTALL)
+        urls = pattern.findall(r.content)
+        for url in urls:
+            if '周' in url and '囧图' in url:
+                pic_url = str(url[len('<a href="'):url.find('" target="_blank">')])
+                join_pic_urls.append(pic_url)
+    return join_pic_urls
+
 
 def main():
-    r = Spider3dm('http://www.3dmgame.com/zt/201612/3612893.html')
-    r.download()
+    root_url = 'http://www.3dmgame.com/zt/'
+    os.chdir('../images')
+    print os.getcwd()
+    urls = get_join_pic_urls(root_url, filename='join_pic_urls.py')
+    for url in urls:
+        r = Spider3dm(url)
+        r.download()
+
 
 if __name__ == '__main__':
     main()
